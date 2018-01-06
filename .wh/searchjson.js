8d1f0f5a8bffa34e4c03b2cc8b147b39ed55f168
Jacob Smith <jacob.smith@unb.ca>
2018-01-06 13:23:51 -0400

    rebuilding site

---
+++ .wh/searchjson.js
@@ -0,0 +1,4 @@
+var tipuesearch = {"pages": [
+{"title":"Webhook","text":"","tags":"","loc":"/cms/"},
+{"title":"Webhook site - Homepage","text":"UNB Programming Rules!\n\n  This is your homepage, located at /pages/index.html.\n\n  Looking for the CMS?","tags":"","loc":"/"},
+]}
